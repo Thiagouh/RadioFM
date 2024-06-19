8eccc310191095943cfef954c1823beccb3a375a
Thiagouh <139012325+Thiagouh@users.noreply.github.com>
2024-06-19 18:17:41 -0300

    text weight and div position changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
     <>
-      <main className="w-screen h-screen flex justify-between flex-col items-center">
+      <main className="w-screen h-screen flex justify-evenly flex-col items-center">
 
         {/* titulo */}
         <header className="flex flex-col items-center gap-2">
-          <h1 className='text-5xl my-shadow mt-3 pl-8 pr-8 pt-8'>Web Rádio</h1>
+          <h1 className='text-5xl font-bold my-shadow mt-3 pl-8 pr-8 pt-8'>WEB RÁDIO</h1>
           <h2 className="text-3xl my-shadow mb-3">o Evangelho de Cristo</h2>
         </header>
 
@@ -18,13 +18,13 @@ export default function Home() {
         </div>
 
         {/* versículo + imagem */}
-        <div className="flex flex-col items-center mb-4">
+      <div className="h-auto flex flex-col items-center bg-amber-950 rounded-lg m-4 p-4">
           <p className='max-w-sm text-lg text-center px-2 pb-6'>Rm 8:1: "Portanto, agora nenhuma condenação há para os que estão em Cristo Jesus."</p>
           <img
             src="/img-bible.png"
             alt="bíblia - imagem"
-            width={80}
-            height={80}
+            width={100}
+            height={100}
             className="rounded-full"
           />
         </div>
